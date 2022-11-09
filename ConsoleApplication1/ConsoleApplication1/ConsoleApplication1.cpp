d05537b07d89f1d945028ceb0bff903d5bba8b11
selmantercioglu <selmantercioglu@gmail.com>
2022-11-09 11:49:43 +0300

    fixed any bugs

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -3,6 +3,11 @@
 #include <string>
 #include <vector>
 #include <list>
+#include <iomanip>
+
+#define PRINT_MATCH false
+#define PRINT_SEARCH false
+
 
 using namespace std;
 
@@ -13,6 +18,29 @@ public:
         file.open(this->file_name, ios::in);
         if (!file.is_open())
             std::cout << "file is not open \n";
+
+        string line{};
+        while (getline(file, line))
+        {
+            lib_vector.push_back({get_libs(line)[0], get_libs(line)[1]});
+        }
+        print_all_vector(true, false, false);
+
+        cout << "vector_size= " << lib_vector.size() << "\n";
+
+
+        for (int i = 0; i < lib_vector.size(); i++)
+        {
+            bool is_delete = search_parent_child(lib_vector[i][0], lib_vector[i][1], i);
+            if (is_delete)
+                lib_vector.erase(lib_vector.begin() + 3);
+        }
+        print_all_vector(true, false, false);
+
+        cout << "vector_size= " << lib_vector.size() << "\n";
+
+        write_to_file("output.txt");
+
     }
 
     static std::string* get_libs(std::string line)
@@ -24,6 +52,8 @@ public:
         // lib2 = GmdssEnums.h
         // return {lib1, lib2};
         */
+
+        //"ChannelHandler.h" -> "GmdssEnums.h"
 
         size_t point1 = line.find("\"");
         size_t point2 = line.find("\"", point1 + 1);
@@ -41,23 +71,132 @@ public:
         return libs;
     }
 
-    int* compare_libs(std::string lib_name)
-    {
 
-    }
-    void add_libs_first_time(string lib1, string lib2, int idx)
+    bool match_parent_child(string parent_lib_name, string child_lib_name)
     {
-        // This function only works once.
-        all_lib_list.push_back({ lib1, lib2 });
-        ef_lib_list = all_lib_list; // copy first lib list from file to efficient lib list.
+        for (int i = 0; i < lib_parent.size(); i++)
+        {
+            for (int k = 0; k < lib_child.size(); k++)
+            {
+                if (lib_child[k][0] == lib_parent[i][1])
+                {
+                    if (PRINT_MATCH)
+                    {
+                        cout << "lib_child  index= " << k << "\n";
+                        cout << "lib_parent index= " << i << "\n\n";
+                    }
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    bool search_parent_child(string parent_lib_name, string child_lib_name, int parent_id)
+    {
+        if (PRINT_SEARCH)
+        {
+            cout << "Searching lib..\n";
+            cout << "parent lib = " << parent_lib_name << "\n";
+            cout << "child lib  = " << child_lib_name << "\n";
+            cout << "index      = " << parent_id << "\n\n";
+        }
+
+        for (int i = 0; i < lib_vector.size(); i++)
+        {
+            if (i == parent_id)
+                continue;
+
+            if (lib_vector[i][0] == parent_lib_name)
+                lib_parent.push_back(lib_vector[i]);
+
+            for (int k = 0; k < lib_vector[i].size(); k++)
+                if (lib_vector[i][k] == child_lib_name)
+                    lib_child.push_back(lib_vector[i]);
+        }
+        return match_parent_child(parent_lib_name, child_lib_name);
+    }
+
+    void print_all_vector(bool all_list, bool parent_list, bool child_list)
+    {
+        if (all_list)
+        {
+            cout << "*********************\n";
+            cout << "All List\n";
+            cout << "*********************\n";
+            for (int i = 0; i < lib_vector.size(); i++) {
+                for (int j = 0; j < lib_vector[i].size(); j++)
+                {
+                    int size = static_cast <int>(lib_vector[i][j].length());
+                    std::cout << "[" << i << "][" << j << "]" << lib_vector[i][j] << setw(50 - size) << " ";
+                }
+                cout << "\n";
+            }
+        }
+        
+        if (parent_list)
+        {
+            cout << "*********************\n";
+            cout << "Parent List\n";
+            cout << "*********************\n";
+            for (int i = 0; i < lib_parent.size(); i++) {
+                for (int j = 0; j < lib_parent[i].size(); j++) {
+                    int size = static_cast <int>(lib_vector[i][j].length());
+                    std::cout << "[" << i << "][" << j << "]" << lib_parent[i][j] << setw(50 - size) << "\t";
+                }
+                cout << "\n";
+            }
+        }
+        if (child_list)
+        {
+            cout << "*********************\n";
+            cout << "Child List\n";
+            cout << "*********************\n";
+            for (int i = 0; i < lib_child.size(); i++) {
+                for (int j = 0; j < lib_child[i].size(); j++)
+                {
+                    int size = static_cast <int>(lib_vector[i][j].length());
+                    std::cout << "[" << i << "][" << j << "]" << lib_child[i][j] << setw(50 - size) << "\t";
+                }
+                cout << "\n";
+            }
+        }
+        cout << "\n\n";
+
+        /*for (int i = 0; i < lib_vector.size(); i++) {
+            for (int j = 0; j < lib_vector[i].size(); j++)
+                std::cout << lib_vector[i][j] << "\t[" << i << "][" << j << "]\n";
+        }*/
     }
 
 private:
-    vector<vector<string>> ef_lib_list; // efficient library list
-    vector<vector<string>> all_lib_list;
+    
+    vector<vector<string>> lib_vector;// efficient library list
+    vector<vector<string>> lib_parent;
+    vector<vector<string>> lib_child;
+
     std::ifstream file;
     std::string file_name{};
 
+
+    void write_to_file(string output_file_name)
+    {
+        ofstream output_file(output_file_name);
+        if (output_file.is_open())
+        {
+            for (int i = 0; i < lib_vector.size(); i++) {
+                for (int j = 0; j < lib_vector[i].size(); j++)
+                {
+                    int size = static_cast <int>(lib_vector[i][j].length());
+                    if(j== 0)
+                        output_file << "\"" << lib_vector[i][j] << "\" -> " << setw(50 - size) << " ";
+                    else
+                        output_file << "\"" << lib_vector[i][j] << "\"" << setw(50 - size) << " ";
+
+                }
+                output_file << "\n";
+            }
+        }
+    }
     std::string get_line_by_line()
     {
         std::string line;
@@ -66,19 +205,17 @@ private:
     }
 };
 
-int find_head_lib(vector<vector<string>>lib_vector)
-{
-    return 0;
-}
+/*
+
+
 void test()
 {
     vector<vector<string>> lib_vector;
-    /*
-    algoritma alternatifleri
-    mehmet'i iceren bir listeye al.
-    bu listedekilerle test'in icerdiklerini karsilastir.
-    ayni olmasi durumunda mehmet'i sil.
-    */
+    //
+    //algoritma alternatifleri
+    //mehmet'i iceren bir listeye al.
+    //bu listedekilerle test'in icerdiklerini karsilastir.
+    //ayni olmasi durumunda mehmet'i sil.
 
     lib_vector.push_back({ "fatih", "mehmet" });
     lib_vector.push_back({ "ali", "mehmet" });
@@ -86,70 +223,131 @@ void test()
     lib_vector.push_back({ "test", "ali" });
     lib_vector.push_back({ "test", "fatma" });
 
-    lib_vector.push_back({ "test", "mehmet" }); 
+    lib_vector.push_back({ "test", "mehmet" });
 
     //lib_vector.erase(lib_vector.begin() + 2);
 }
+
+vector<vector<string>> lib_vector;
+vector<vector<string>> lib_parent;
+vector<vector<string>> lib_child;
+
+bool match_parent_child(string parent_lib_name, string child_lib_name)
+{
+    for (int i = 0; i < lib_parent.size(); i++)
+    {
+        for (int k = 0; k < lib_child.size(); k++)
+        {
+            if (lib_child[k][0] == lib_parent[i][1])
+            {
+                if (PRINT_MATCH)
+                {
+                    cout << "lib_child  index= " << k << "\n";
+                    cout << "lib_parent index= " << i << "\n\n";
+                }
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+bool search_parent_child(string parent_lib_name, string child_lib_name, int parent_id)
+{
+    if (PRINT_SEARCH)
+    {
+        cout << "Searching lib..\n";
+        cout << "parent lib = " << parent_lib_name << "\n";
+        cout << "child lib  = " << child_lib_name << "\n";
+        cout << "index      = " << parent_id << "\n\n";
+    }
+
+    for (int i = 0; i < lib_vector.size(); i++)
+    {
+        if (i == parent_id)
+            continue;
+
+        if (lib_vector[i][0] == parent_lib_name)
+            lib_parent.push_back(lib_vector[i]);
+
+        for (int k = 0; k < lib_vector[i].size(); k++)
+            if (lib_vector[i][k] == child_lib_name)
+                lib_child.push_back(lib_vector[i]);
+    }
+    return match_parent_child(parent_lib_name, child_lib_name);
+}
+void test_push_back()
+{
+    lib_vector.push_back({ "fatih", "mehmet" });
+    lib_vector.push_back({ "ali", "mehmet" });
+    lib_vector.push_back({ "test", "ali" });
+    lib_vector.push_back({ "test", "mehmet" }); // unneccessary 
+    lib_vector.push_back({ "test", "ayse" });   // unneccessary 
+    lib_vector.push_back({ "fatih", "ayse" });
+}
+
+void print_all_vector(bool all_list, bool parent_list, bool child_list)
+{
+    if (all_list)
+    {
+        cout << "all list\n";
+        for (int i = 0; i < lib_vector.size(); i++) {
+            for (int j = 0; j < lib_vector[i].size(); j++)
+                std::cout << "[" << i << "][" << j << "]" << lib_vector[i][j] << "\t";
+            cout << "\n";
+        }
+    }
+    if (parent_list)
+    {
+        cout << "\nparent list\n";
+        for (int i = 0; i < lib_parent.size(); i++) {
+            for (int j = 0; j < lib_parent[i].size(); j++)
+                std::cout << "[" << i << "][" << j << "]" << lib_parent[i][j] << "\t";
+            cout << "\n";
+        }
+    }
+    if (child_list)
+    {
+        cout << "\nchild list\n";
+        for (int i = 0; i < lib_child.size(); i++) {
+            for (int j = 0; j < lib_child[i].size(); j++)
+                std::cout << "[" << i << "][" << j << "]" << lib_child[i][j] << "\t";
+            cout << "\n";
+        }
+    }
+    cout << "\n\n";
+
+    //for (int i = 0; i < lib_vector.size(); i++) {
+    //    for (int j = 0; j < lib_vector[i].size(); j++)
+    //        std::cout << lib_vector[i][j] << "\t[" << i << "][" << j << "]\n";
+    //}
+
+}
+
 int main()
 {
     //std::string line =  "\"ChannelHandler.h\" -> \"GmdssEnums.h\"";
     //Efficiency x("dependency.txt");
 
-    //lib_vector[0].push_back("oglu");
 
-    vector<vector<string>> lib_vector;
+    //test_push_back();
+    //
+    //print_all_vector(true,false,false);
 
-    /*
-    algoritma alternatifleri
-    mehmet'i iceren bir listeye al.
-    bu listedekilerle test'in icerdiklerini karsilastir.
-    ayni olmasi durumunda mehmet'i sil.
-    */
+    //for (int i = 0; i < lib_vector.size(); i++)
+    //{
+    //    bool is_delete = search_parent_child(lib_vector[i][0], lib_vector[i][1], i);
+    //    if (is_delete)
+    //        lib_vector.erase(lib_vector.begin() + 3);
+    //}
 
-    lib_vector.push_back({"fatih", "mehmet"});
+    //print_all_vector(true,false,false);
+}
 
-    lib_vector.push_back({"ali", "mehmet"});
-    lib_vector.push_back({"test", "ali"});
-    lib_vector.push_back({"test", "mehmet"}); // test >> mehmet "delete mehmet"
+*/
 
-    lib_vector.erase(lib_vector.begin() + 2);
-
-    //lib_vector.erase(lib_vector.begin() + 2);
-
-    std::string find_text = "deneme";
-    
-    int max = 0;
-    vector<int> temp;
-
-    // find head has max member lib test code
-    cout << "\n\n";
-    for (int i = 0; i < lib_vector.size(); i++) {
-        if (lib_vector[i][0] == find_text)
-        {
-            temp.push_back(i);
-            if (lib_vector[i].size() > max)
-                max = i;
-            cout << "BULUNDU! [" << i << "]"<< lib_vector[i][0] << "\n";
-        }
-    }
-    cout << "max= " << max << "\n";
-
-    for (int i = 0; i < temp.size(); i++)
-    {
-        if (i != max) 
-            lib_vector.erase(lib_vector.begin() + i);
-    }
-
-    for (int i = 0; i < lib_vector.size(); i++) {
-        for (int j = 0; j < lib_vector[i].size(); j++)
-            std::cout << "[" << i << "][" << j << "]" << lib_vector[i][j] << "\t";
-        cout << "\n";
-    }
-
-    /*for (int i = 0; i < lib_vector.size(); i++) {
-        for (int j = 0; j < lib_vector[i].size(); j++)
-            std::cout << lib_vector[i][j] << "\t[" << i << "][" << j << "]\n";
-    }*/
-
+int main()
+{
+    //Efficiency x("dependency.txt");
 
 }
